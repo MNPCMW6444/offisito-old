80c7a46641df82b17356097d75285515ae8f7ddf
Michael Nusair <mnpcmw6444@gmail.com>
2024-01-12 15:57:43 +0200

    package.json slow migration

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,5 @@
-import expressSetup from "./app/express/expressSetup";
-import {connect} from "./app/mongo/connection";
+import expressSetup from './app/express/expressSetup';
+import { connect } from './app/mongo/connection';
 
-console.log("Starting server but first connecting to mongo,...")
-connect().then(() => expressSetup().catch((e) => console.log(e)))
-
+console.log('Starting server but first connecting to mongo,...');
+connect().then(() => expressSetup().catch((e) => console.log(e)));
