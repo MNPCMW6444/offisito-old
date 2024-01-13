1c5ba1803423f823eb10b85100e011d7789d1c7a
Michael Nusair <MNPCMW6444@gmail.com>
2024-01-13 14:21:56 +0200

    guest init

---
+++ apps/host/src/app/components/sapce-card/SpaceCard.tsx
@@ -0,0 +1,13 @@
+import { Card, Grid } from "@mui/material";
+
+const spaceCard = () => (
+  <Card>
+    <Grid borderRadius="5px" container direction="column">
+      <Grid item>a</Grid>
+      <Grid item>a</Grid>
+      <Grid item>a</Grid>
+    </Grid>
+  </Card>
+);
+
+export default spaceCard;
