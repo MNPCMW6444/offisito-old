c77fb2826edda808d9c638f3231c22271e06fc20
Michael Nusair <MNPCMW6444@gmail.com>
2024-01-18 15:55:02 +0200

    sendEmail refactor

--- apps/server/src/app/sendgrid/sendEmail.ts
+++ apps/server/src/app/sendgrid/sendEmail.ts
@@ -5,14 +5,14 @@ import * as process from "process";
 dotenv.config();
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
-    sgMail.setApiKey(process.env.SENDGRID + "");
-    return await sgMail.send({
-        from: {
-            email: "service@failean.com",
-            name: "name.up",
-        },
-        to,
-        subject,
-        html,
-    });
+  sgMail.setApiKey(process.env.SENDGRID + "");
+  return await sgMail.send({
+    from: {
+      email: "service@offisito.com",
+      name: "name.up",
+    },
+    to,
+    subject,
+    html,
+  });
 };
