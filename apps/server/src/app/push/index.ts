506632c8baf7557000cfd2194ff67bbc9c19c958
Michael Nusair <MNPCMW6444@gmail.com>
2024-01-20 18:01:39 +0200

    ci/cd

--- apps/server/src/app/push/index.ts
+++ apps/server/src/app/push/index.ts
@@ -1,8 +1,9 @@
+/*
 import webpush from "web-push";
 //import settings from "../../config";
 
 // VAPID keys should be generated once and stored securely
-/*const vapidKeys = {
+/!*const vapidKeys = {
     publicKey: 'BOX9mgkzgqKdn0j6vi-86nqWXoo24Ir4NAPwLe3M-lHgZpBLT153asOtuX1ocALmL3aRzBWgoRhjDAC80-llb6g',
     privateKey: settings?.pushPrivate
 };
@@ -13,7 +14,7 @@ webpush.setVapidDetails(
     vapidKeys.privateKey || (() => {
         throw new Error('No private key set')
     })()
-);*/
+);*!/
 
 export const sendPushNotification = async (
   subscription: any,
@@ -30,3 +31,4 @@ export const sendPushNotification = async (
     console.error("Error sending notification:", error);
   }
 };
+*/
