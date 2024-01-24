7203ac83d9d427f86e3ee2a338254d858763e6c4
Michael Nusair <MNPCMW6444@gmail.com>
2024-01-24 14:02:53 +0200

    testing a change

--- apps/server/src/app/express/api/auth/routers/logRouter.ts
+++ apps/server/src/app/express/api/auth/routers/logRouter.ts
@@ -1,16 +1,16 @@
-import { Router } from 'express';
-import userModel from '../../../../mongo/auth/userModel';
-import requestForAccountModel from '../../../../mongo/auth/requestForAccountModel';
-import settings from '../../../../../config';
-import bcrypt from 'bcrypt';
-import jsonwebtoken from 'jsonwebtoken';
-import { authUser } from '../authUtil';
+import { Router } from "express";
+import userModel from "../../../../mongo/auth/userModel";
+import requestForAccountModel from "../../../../mongo/auth/requestForAccountModel";
+import settings from "../../../../../config";
+import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
+import { authUser } from "../authUtil";
 // import { GetInReq, GetInRes, PostInReq, PostInRes } from "@monorepo/types";
 
 const router = Router();
 //const MIN_PASSWORD_STRENGTH = 3;
 
-router.get('/in', async (req, res) => {
+router.get("/inakjsdkjasdasd", async (req, res) => {
   const User = userModel();
   if (User)
     try {
@@ -18,103 +18,103 @@ router.get('/in', async (req, res) => {
       if (!user) {
         return res
           .status(401)
-          .json({ errorMessage: 'Unauthorized. Try to login (post)' });
+          .json({ errorMessage: "Unauthorized. Try to login (post)" });
       }
       delete user.passwordHash;
       return res.json({ user });
     } catch (err) {
       return res
         .status(401)
-        .json({ errorMessage: 'Unauthorized. Try to login (post)' });
+        .json({ errorMessage: "Unauthorized. Try to login (post)" });
     }
   else
     return res
       .status(500)
-      .json({ errorMessage: 'Server Error nichal todo api' });
+      .json({ errorMessage: "Server Error nichal todo api" });
 });
 
-router.post('/in', async (req, res) => {
+router.post("/in", async (req, res) => {
   const User = userModel();
   const RequestForAccount = requestForAccountModel();
   if (User && RequestForAccount) {
     try {
       const { email, password } = req.body;
       if (!email) {
-        return res.status(400).json({ errorMessage: 'Must pass an email' });
+        return res.status(400).json({ errorMessage: "Must pass an email" });
       }
       const existingUser = await User.findOne({ email });
       if (!existingUser) {
         return res.status(402).json({
-          errorMessage: 'please register'
+          errorMessage: "please register",
         });
       }
       const correctPassword = await bcrypt.compare(
         password,
-        existingUser.passwordHash
+        existingUser.passwordHash,
       );
       if (!correctPassword) {
         return res.status(401).json({
-          errorMessage: 'Wrong email or password'
+          errorMessage: "Wrong email or password",
         });
       }
       const token = jsonwebtoken.sign(
         {
-          id: existingUser._id
+          id: existingUser._id,
         },
-        process.env.JWT + ''
+        process.env.JWT + "",
       );
       return res
-        .cookie('jsonwebtoken', token, {
+        .cookie("jsonwebtoken", token, {
           httpOnly: true,
           sameSite:
-            (settings.nodeEnv === 'development'
-              ? 'lax'
-              : settings.nodeEnv === 'production' && 'none') || false,
+            (settings.nodeEnv === "development"
+              ? "lax"
+              : settings.nodeEnv === "production" && "none") || false,
           secure:
-            settings.nodeEnv === 'development'
+            settings.nodeEnv === "development"
               ? false
-              : settings.nodeEnv === 'production' && true
+              : settings.nodeEnv === "production" && true,
         })
         .send();
     } catch (err) {
       console.error(err);
       return res
         .status(500)
-        .json({ errorMessage: 'Unexpected error occurred in the server' });
+        .json({ errorMessage: "Unexpected error occurred in the server" });
     }
   } else
     return res
       .status(500)
-      .json({ errorMessage: 'Server Error nichal todo api' });
+      .json({ errorMessage: "Server Error nichal todo api" });
 });
 
-router.get('/out', async (_, res) => {
+router.get("/out", async (_, res) => {
   const User = userModel();
   if (User)
     try {
       return res
-        .cookie('jsonwebtoken', '', {
+        .cookie("jsonwebtoken", "", {
           httpOnly: true,
           sameSite:
-            (settings.nodeEnv === 'development'
-              ? 'lax'
-              : settings.nodeEnv === 'production' && 'none') || false,
+            (settings.nodeEnv === "development"
+              ? "lax"
+              : settings.nodeEnv === "production" && "none") || false,
           secure:
-            settings.nodeEnv === 'development'
+            settings.nodeEnv === "development"
               ? false
-              : settings.nodeEnv === 'production' && true,
-          expires: new Date(0)
+              : settings.nodeEnv === "production" && true,
+          expires: new Date(0),
         })
         .send();
     } catch (err) {
       return res
         .status(500)
-        .json({ errorMessage: 'Server Error nichal todo api' });
+        .json({ errorMessage: "Server Error nichal todo api" });
     }
   else
     return res
       .status(500)
-      .json({ errorMessage: 'Server Error nichal todo api' });
+      .json({ errorMessage: "Server Error nichal todo api" });
 });
 
 export default router;
