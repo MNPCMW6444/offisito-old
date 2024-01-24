fa574eca1801e2487ac7c078e5435dcfa6d11805
GitHub Action <michael+cicd@offisito.com>
2024-01-24 12:04:24 +0000

    chore(release): bump version to 0.2.16 [skip ci]

--- libs/types/src/api/auto/autoApi.ts
+++ libs/types/src/api/auto/autoApi.ts
@@ -255,12 +255,12 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
     /**
      * No description
      *
-     * @name AuthLogInList
-     * @request GET:/api/auth/log/in
+     * @name AuthLogInakjsdkjasdasdList
+     * @request GET:/api/auth/log/inakjsdkjasdasd
      */
-    authLogInList: (params: RequestParams = {}) =>
+    authLogInakjsdkjasdasdList: (params: RequestParams = {}) =>
       this.request<void, void>({
-        path: `/api/auth/log/in`,
+        path: `/api/auth/log/inakjsdkjasdasd`,
         method: "GET",
         ...params,
       }),
