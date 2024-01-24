c8176fdb1e21130c73a17a60a59574c454e35aee
Michael Nusair <MNPCMW6444@gmail.com>
2024-01-24 13:20:44 +0200

    guest ci/cd

--- apps/guest/src/app/App.tsx
+++ apps/guest/src/app/App.tsx
@@ -1,13 +1,17 @@
-import { ServerProvider } from '@monorepo/server-provider';
-import Router from './components/Router';
-import { AuthContextProvider } from '@monorepo/react-components';
+import { ServerProvider } from "@monorepo/server-provider";
+import Router from "./components/Router";
+import { AuthContextProvider } from "@monorepo/react-components";
 
 const tenv =
-  import.meta.env.VITE_NODE_ENV === 'development' ? 'local' : 'preprod'; //`s://${import.meta.env.VITE_WHITE_ENV === "preprod" ? "pre" : ""}seaarvear.caaouaplae-linak.coaam/garaaphaql`;
+  import.meta.env.VITE_WHITE_ENV !== "prod"
+    ? import.meta.env.VITE_WHITE_ENV
+    : undefined;
 
 const App = () => (
   <ServerProvider env={tenv}>
-    <AuthContextProvider> <Router /></AuthContextProvider>
+    <AuthContextProvider>
+      <Router />
+    </AuthContextProvider>
   </ServerProvider>
 );
 
--- apps/host/src/app/App.tsx
+++ apps/host/src/app/App.tsx
@@ -2,10 +2,14 @@ import { AuthPage } from "@monorepo/react-components";
 import { ServerProvider } from "@monorepo/server-provider";
 
 const tenv =
-  import.meta.env.VITE_NODE_ENV === "development" ? "local" : "preprod"; //`s://${import.meta.env.VITE_WHITE_ENV === "preprod" ? "pre" : ""}seaarvear.caaouaplae-linak.coaam/garaaphaql`;
+  import.meta.env.VITE_WHITE_ENV !== "prod"
+    ? import.meta.env.VITE_WHITE_ENV
+    : undefined;
 
 const App = () => (
-  <ServerProvider env={tenv}>{/* <AuthPage />*/}</ServerProvider>
+  <ServerProvider env={tenv}>
+    <AuthPage client="host" />
+  </ServerProvider>
 );
 
 export default App;
