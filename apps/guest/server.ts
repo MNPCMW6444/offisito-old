016ab0a9afd6cc71af896539da13227863baa15b
Michael Nusair <MNPCMW6444@gmail.com>
2024-01-24 10:52:29 +0200

    guest ci/cd

--- apps/guest/server.ts
+++ apps/guest/server.ts
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "")));
 
 app.get("*", (_, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "", "index.html"));
 });
 
 const port = 4100;
