d103530e99f7d39c8e41490984cb38d6b76eac75
Michael Nusair <MNPCMW6444@gmail.com>
2024-01-24 10:48:41 +0200

    guest ci/cd

---
+++ apps/guest/server.ts
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+const port = 4100;
+app.listen(port, "0.0.0.0");
+
+console.log("App is listening on port " + port);
