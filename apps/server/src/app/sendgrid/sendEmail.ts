fc1d560ee1fb088597b4d7ff2a77b8e00e014846
Michael Nusair <MNPCMW6444@gmail.com>
2024-01-24 22:27:12 +0200

    api

--- apps/server/src/app/sendgrid/sendEmail.ts
+++ apps/server/src/app/sendgrid/sendEmail.ts
@@ -1,19 +1,20 @@
-import sgMail from '@sendgrid/mail';
-import dotenv from 'dotenv';
-import settings from '../../config';
-import Name from '../../assets/name';
+import sgMail from "@sendgrid/mail";
+import dotenv from "dotenv";
+import settings from "../../config";
+import Name from "../../assets/name";
 
 dotenv.config();
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
+  settings.nodeEnv === "development" && console.log(html);
   sgMail.setApiKey(settings.sendgridApiKey);
   return await sgMail.send({
     from: {
-      email: 'service@offisito.com',
-      name: Name.up
+      email: "service@offisito.com",
+      name: Name.up,
     },
     to,
     subject,
-    html
+    html,
   });
 };
