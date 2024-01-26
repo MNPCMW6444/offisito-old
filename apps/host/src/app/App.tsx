90be08e055153e9f9bc68bde5bbf9c155de98016
Michael Nusair <MNPCMW6444@gmail.com>
2024-01-26 18:19:11 +0200

    f

--- apps/host/src/app/App.tsx
+++ apps/host/src/app/App.tsx
@@ -2,13 +2,13 @@ import { ServerProvider } from "@monorepo/server-provider";
 import { AuthContextProvider } from "@monorepo/react-components";
 import Router from "./components/Router";
 
-const tenv =
+const whiteEnv =
   import.meta.env.VITE_WHITE_ENV !== "prod"
     ? import.meta.env.VITE_WHITE_ENV
     : undefined;
 
 const App = () => (
-  <ServerProvider env={tenv}>
+  <ServerProvider env={whiteEnv}>
     <AuthContextProvider>
       <Router />
     </AuthContextProvider>
