cdf8655c269b1dcb3273aa5ec489feec0fd92d0e
Michael Nusair <MNPCMW6444@gmail.com>
2024-01-27 18:55:53 +0200

    michaels comments

--- apps/server/src/app/express/api/assets/assetsRouter.ts
+++ apps/server/src/app/express/api/assets/assetsRouter.ts
@@ -1,8 +1,31 @@
 import { Router } from "express";
 import { createAsset } from "./assetsController";
+import { Asset } from "@monorepo/types";
+import assetModel from "../../../mongo/assets/assetModel";
 
 const assetsRouter = Router();
 
 assetsRouter.post("/create", createAsset);
+
+assetsRouter.get<{ _id: string; location: string }, Asset[]>(
+  "/:_id",
+  async (req, res) => {
+    try {
+      const Asset = assetModel();
+      if (Asset) {
+        const assets = req.params._id
+          ? [await Asset.findById(req.params._id)]
+          : await Asset.find(/*{ params }*/);
+        if (!assets) {
+          return res.status(404).send();
+        }
+        res.json(assets);
+      } else throw new Error("no db");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send();
+    }
+  },
+);
 
 export default assetsRouter;
