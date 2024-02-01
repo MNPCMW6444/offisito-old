dd48ae5344736ce9c9ca24ed059565626397d06b
Michael Nusair <MNPCMW6444@gmail.com>
2024-02-01 20:05:34 +0200

    address locationing test

--- apps/server/src/app/google-geocoding/index.ts
+++ apps/server/src/app/google-geocoding/index.ts
@@ -2,14 +2,6 @@ import settings from "../../config";
 import axios from "axios";
 
 export const getAddressByPoint = async (lat: number, long: number) => {
-  console.log(
-    "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
-      lat +
-      "," +
-      long +
-      "&key=" +
-      settings.googleGeoCoding,
-  );
   try {
     const res = await axios.get(
       "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
