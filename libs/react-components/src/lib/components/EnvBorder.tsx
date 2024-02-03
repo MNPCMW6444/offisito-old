b65ab504f584c6a6360f683c9d51ed322aa1bd4a
Michael Nusair <MNPCMW6444@gmail.com>
2024-02-03 19:37:37 +0200

    test of &

--- libs/react-components/src/lib/components/EnvBorder.tsx
+++ libs/react-components/src/lib/components/EnvBorder.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Box from "@mui/material/Box";
 import { frontendSettings } from "@monorepo/utils";
 
@@ -7,6 +7,24 @@ interface EnvBorderContextProps {
 }
 
 export const EnvBorder = ({ children }: EnvBorderContextProps) => {
+  useEffect(() => {
+    const adjustHeight = () => {
+      const viewportHeight = window.innerHeight + "px";
+      if (document.getElementById("root") !== null)
+        (document.getElementById("root") as HTMLElement).style.height =
+          viewportHeight;
+    };
+
+    // Adjust the height on initial render
+    adjustHeight();
+
+    // Add event listener to adjust the height on window resize
+    window.addEventListener("resize", adjustHeight);
+
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener("resize", adjustHeight);
+  }, []);
+
   const props = {
     width:
       frontendSettings().VITE_WHITE_ENV === "prod"
