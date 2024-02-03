d9f579bd37278cf2e85175ba96fb70da94e2b329
Michael Nusair <MNPCMW6444@gmail.com>
2024-02-03 18:48:37 +0200

    env border

--- apps/guest/server.ts
+++ apps/guest/server.ts
@@ -3,26 +3,28 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
+const indexPath = path.join(__dirname, "index.html");
 
-app.use(express.static(path.join(__dirname, ""))); // Assuming your static files are in a '' directory
+// Serve static files but exclude index.html
+app.use(express.static(path.join(__dirname), { index: false }));
 
 app.get("*", (req, res) => {
-  const indexPath = path.join(__dirname, "", "index.html");
   fs.readFile(indexPath, "utf8", (err, htmlData) => {
     if (err) {
       console.error("Error during file reading", err);
-      return res.status(404).end();
+      return res.status(500).end();
     }
 
-    // Here you define your environment variables
+    // Define your environment variables here
     const envVariables = JSON.stringify({
       VITE_NODE_ENV: process.env.VITE_NODE_ENV || "production",
       VITE_WHITE_ENV: process.env.VITE_WHITE_ENV || "prod",
     });
 
-    // Replace the placeholder with actual environment variables
+    // Ensure the replacement string matches the placeholder in your HTML
     const finalHtml = htmlData.replace("%ENV_VARIABLES%", envVariables);
 
+    // Serve the modified HTML
     res.send(finalHtml);
   });
 });
