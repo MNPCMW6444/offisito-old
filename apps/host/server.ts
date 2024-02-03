9a062e30bfef862ef1968dd0d9135ba516067039
Michael Nusair <MNPCMW6444@gmail.com>
2024-02-03 14:30:30 +0200

    env border

--- apps/host/server.ts
+++ apps/host/server.ts
@@ -1,12 +1,24 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const dotenv = require("dotenv");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "")));
 
 app.get("*", (_, res) => {
-  res.sendFile(path.join(__dirname, "", "index.html"));
+  const indexPath = path.join(__dirname, "", "index.html");
+  let indexHTML = fs.readFileSync(indexPath, "utf8");
+
+  // Assuming you want to pass process.env.API_URL to the frontend
+  const envVariables = { API_URL: process.env.API_URL };
+  const envScript = `<script>window.env = ${JSON.stringify(envVariables)};</script>`;
+
+  // Replace the placeholder with actual env variables
+  indexHTML = indexHTML.replace("<script>window.env = {};</script>", envScript);
+
+  res.send(indexHTML);
 });
 
 const port = 4200;
