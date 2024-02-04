f1e3e3aaed83f2c07ed2d9dae9b06a3a09497f49
Michael Nusair <MNPCMW6444@gmail.com>
2024-02-04 11:48:27 +0200

    host frontend

--- apps/host/src/app/components/pages/home/Dashboard.tsx
+++ apps/host/src/app/components/pages/home/Dashboard.tsx
@@ -1,37 +1,52 @@
-import { Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-const Dashboard = () => (
-  <Grid
-    container
-    direction="column"
-    alignItems="center"
-    height="100%"
-    width="100%"
-  >
-    <Grid item alignSelf="flex-start">
-      <Typography>My Dashboard</Typography>
-    </Grid>
-    <Grid item container justifyContent="space-around" alignItems="center">
-      <Grid item>
-        <Typography>Stats</Typography>
-      </Grid>
-      <Grid item>
-        <Typography>Stats</Typography>
-      </Grid>
-      <Grid item>
-        <Typography>Stats</Typography>
-      </Grid>
-      <Grid item>
-        <Typography>Stats</Typography>
-      </Grid>
-    </Grid>
-    <Grid item>
-      <Typography>My Dashboard</Typography>
-    </Grid>
-    <Grid item>
-      <Typography>My Dashboard</Typography>
-    </Grid>
-  </Grid>
-);
+const Dashboard = () => {
+  const navigate = useNavigate();
 
+  return (
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      height="100%"
+      width="100%"
+    >
+      <Grid item alignSelf="flex-start">
+        <Typography>My Dashboard</Typography>
+      </Grid>
+      <Grid item container justifyContent="space-around" alignItems="center">
+        <Grid item>
+          <Typography>Stats</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Stats</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Stats</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Stats</Typography>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Typography>Nav:</Typography>
+      </Grid>
+      <Grid item container justifyContent="space-around" alignItems="center">
+        <Grid item>
+          <Button onClick={() => navigate("/spaces")}>My Spaces</Button>
+        </Grid>
+        <Grid item>
+          <Typography>Stats</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Stats</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Stats</Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 export default Dashboard;
