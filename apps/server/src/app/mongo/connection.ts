630875429541bfbba57f427e3041169fa3735a8d
GitHub Action <michael+cicd@offisito.com>
2024-02-07 00:15:53 +0200

    debug only on local

--- apps/server/src/app/mongo/connection.ts
+++ apps/server/src/app/mongo/connection.ts
@@ -4,15 +4,16 @@ import settings from "../../config";
 export let connection: mongoose.Connection | null = null;
 
 export const connect = async () => {
-    console.log("***********CONNECTED ");
-    mongoose.set('debug', true);
-    try {
-        await mongoose.connect(settings.mongoURI || "mongodb://localhost:27017/error");
-        console.log("Mongo DB connected successfully");
-        connection = mongoose.connection;
-    } catch (err) {
-        console.error("mongo connection error:" + err.message);
-        throw new Error(err);
-    }
+  console.log("***********CONNECTED ");
+  settings.nodeEnv === "development" && mongoose.set("debug", true);
+  try {
+    await mongoose.connect(
+      settings.mongoURI || "mongodb://localhost:27017/error",
+    );
+    console.log("Mongo DB connected successfully");
+    connection = mongoose.connection;
+  } catch (err) {
+    console.error("mongo connection error:" + err.message);
+    throw new Error(err);
+  }
 };
-
