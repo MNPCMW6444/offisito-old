9efbe85c7583d42ddd8c05436909e9c035a6fa49
GitHub Action <michael+cicd@offisito.com>
2024-02-09 18:34:49 +0200

    host frontend - profile completed, moving to asset

--- apps/host/src/app/components/pages/profiles/ProfileForm.tsx
+++ apps/host/src/app/components/pages/profiles/ProfileForm.tsx
@@ -1,28 +1,18 @@
-import {
-  FormControlLabel,
-  Grid,
-  MenuItem,
-  Select,
-  Switch,
-  TextField,
-} from "@mui/material";
-import {
-  ChangeEvent,
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { Grid } from "@mui/material";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { ServerContext } from "@monorepo/server-provider";
-import { Company, CreateEditCompanyReq } from "@monorepo/types";
-import { axiosErrorToaster } from "@monorepo/react-components";
+import { Company } from "@monorepo/types";
+import {
+  axiosErrorToaster,
+  renderDatePicker,
+  renderDropdown,
+  renderSwitch,
+  renderTextField,
+} from "@monorepo/react-components";
 import { useLocation } from "react-router-dom";
 import debounce from "lodash.debounce";
 import { PrimaryText } from "@monorepo/react-styles";
 import toast from "react-hot-toast";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const ProfileForm = () => {
   const [formState, setFormState] = useState<Company>();
@@ -107,67 +97,6 @@ const ProfileForm = () => {
       }) as any);
   };
 
-  const renderTextField = (name: keyof CreateEditCompanyReq, label: string) => (
-    <TextField
-      multiline
-      variant="outlined"
-      label={label}
-      value={formState ? formState[name] : ""}
-      onChange={(e: ChangeEvent<HTMLInputElement>) => {
-        handleChange(name, e.target.value);
-      }}
-      name={name}
-    />
-  );
-
-  const renderSwitch = (name: keyof CreateEditCompanyReq, label: string) => (
-    <FormControlLabel
-      control={
-        <Switch
-          checked={!!formState?.[name]}
-          onChange={(e: any) => {
-            handleChange(name, e.target.checked);
-          }}
-          name={name}
-        />
-      }
-      label={label}
-    />
-  );
-
-  const renderDatePicker = (
-    name: keyof CreateEditCompanyReq,
-    label: string,
-  ) => (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker
-        label={label}
-        value={formState ? formState[name] : ""}
-        onChange={(date) => {
-          typeof date === "object" && handleChange(name, date as Date);
-        }}
-        name={name}
-      />
-    </LocalizationProvider>
-  );
-
-  const renderDropdown = (
-    name: keyof CreateEditCompanyReq,
-    label: string,
-    options: { value: string; label?: string }[],
-  ) => (
-    <Select
-      name={name}
-      label={label}
-      value={formState ? formState[name] : ""}
-      onChange={(e) => handleChange(name, e.target.value as string)}
-    >
-      {options.map(({ label, value }) => (
-        <MenuItem value={value}>{label || value}</MenuItem>
-      ))}
-    </Select>
-  );
-
   return formState?._id ? (
     <Grid
       container
@@ -182,20 +111,58 @@ const ProfileForm = () => {
       <Grid item>
         <PrimaryText variant="h4">Company Profile</PrimaryText>
       </Grid>
-      <Grid item>{renderTextField("companyName", "Company Name")}</Grid>
-      <Grid item>{renderTextField("companyInHold", "Company in Hold")}</Grid>
-      <Grid item>{renderTextField("floorNumber", "Floor Number")}</Grid>
-      <Grid item>{renderSwitch("fullFloor", "Full Floor?")}</Grid>
       <Grid item>
-        {renderDatePicker("contractEndDate", "Contract End Date")}
+        {renderTextField(
+          formState,
+          handleChange,
+          "companyName",
+          "Company Name",
+        )}
+      </Grid>
+      <Grid item>
+        {renderTextField(
+          formState,
+          handleChange,
+          "companyInHold",
+          "Company in Hold",
+        )}
+      </Grid>
+      <Grid item>
+        {renderTextField(
+          formState,
+          handleChange,
+          "floorNumber",
+          "Floor Number",
+        )}
+      </Grid>
+      <Grid item>
+        {renderSwitch(formState, handleChange, "fullFloor", "Full Floor?")}
+      </Grid>
+      <Grid item>
+        {renderDatePicker(
+          formState,
+          handleChange,
+          "contractEndDate",
+          "Contract End Date",
+        )}
       </Grid>
       <Grid item>
         {renderSwitch(
+          formState,
+          handleChange,
           "subleasePermission",
           "I have a legal sublease permission",
         )}
       </Grid>
-      <Grid item>{renderDropdown("building", "Building", buildings)}</Grid>
+      <Grid item>
+        {renderDropdown(
+          formState,
+          handleChange,
+          "building",
+          "Building",
+          buildings,
+        )}
+      </Grid>
     </Grid>
   ) : (
     <PrimaryText>{hasFetched.current ? "Error" : "Loading..."}</PrimaryText>
---
+++ libs/react-components/src/lib/components/form-parts/index.tsx
@@ -0,0 +1,96 @@
+import {
+  FormControlLabel,
+  MenuItem,
+  Select,
+  Switch,
+  TextField,
+} from "@mui/material";
+import { ChangeEvent } from "react";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+
+export * from "./switches";
+export * from "./labels";
+
+export const renderTextField = <T,>(
+  formState: T,
+  handleChange: (name: keyof T, value: string | Date) => void,
+  name: keyof T,
+  label: string,
+) => (
+  <TextField
+    multiline
+    variant="outlined"
+    label={label}
+    value={formState ? formState[name] : ""}
+    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+      handleChange(name, e.target.value);
+    }}
+    name={name as string}
+  />
+);
+
+export const renderSwitch = <T,>(
+  formState: T,
+  handleChange: (name: keyof T, value: string | Date) => void,
+  name: keyof T,
+  label: string,
+) => (
+  <FormControlLabel
+    sx={(theme) => ({ color: theme.palette.primary.main })}
+    control={
+      <Switch
+        checked={!!formState?.[name]}
+        onChange={(e: any) => {
+          handleChange(name, e.target.checked);
+        }}
+        name={name as string}
+      />
+    }
+    label={label}
+  />
+);
+
+export const renderDatePicker = <T,>(
+  formState: T,
+  handleChange: (name: keyof T, value: string | Date) => void,
+  name: keyof T,
+  label: string,
+) => (
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <DatePicker
+      label={label}
+      value={dayjs(
+        formState && formState[name]
+          ? new Date(formState ? (formState[name] as Date) : Date.now())
+          : new Date(),
+      )}
+      onChange={(newDate) =>
+        handleChange(name, newDate ? new Date(newDate.valueOf()) : new Date())
+      }
+      name={name as string}
+    />
+  </LocalizationProvider>
+);
+
+export const renderDropdown = <T,>(
+  formState: T,
+  handleChange: (name: keyof T, value: string | Date) => void,
+  name: keyof T,
+  label: string,
+  options: { value: string | Date; label?: string }[],
+) => (
+  <Select
+    name={name as string}
+    label={label}
+    value={formState ? formState[name] : ""}
+    onChange={(e) => handleChange(name, e.target.value as string)}
+  >
+    {options.map(({ label, value }) => (
+      <MenuItem key={value as string} value={value as string}>
+        {label || (value as string)}
+      </MenuItem>
+    ))}
+  </Select>
+);
