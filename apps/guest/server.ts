99b0806eeabf09ca6d3cbfd2df2a1b8ca04f8bbd
GitHub Action <michael+cicd@offisito.com>
2024-02-10 22:37:27 +0200

    logos

--- apps/guest/server.ts
+++ apps/guest/server.ts
@@ -1,23 +1,61 @@
-// Import types
 import { Request, Response } from "express";
 
 const { manifest } = require("./pws");
-
-// CommonJS require for modules
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 require("dotenv").config();
 
 const app = express();
-const indexPath = path.join(__dirname, "index.html");
+const indexPath = path.join(__dirname, "../../../guest/", "index.html");
 
-app.use(express.static(path.join(__dirname), { index: false }));
+app.use(
+  express.static(path.join(__dirname, "../../../guest/"), { index: false }),
+);
 
 const envVars = {
   VITE_NODE_ENV: process.env.VITE_NODE_ENV || "production",
   VITE_WHITE_ENV: process.env.VITE_WHITE_ENV || "prod",
 };
+
+app.get("/manifest.json", (req: Request, res: Response) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(
+    JSON.stringify(
+      manifest(envVars.VITE_WHITE_ENV === "preprod" ? "PreProd" : undefined),
+    ),
+  );
+});
+
+app.get("/icons/l144.png", (req: Request, res: Response) => {
+  const imagePath = path.join(
+    __dirname,
+    "icons",
+    envVars.VITE_WHITE_ENV === "prod" ? "" : envVars.VITE_WHITE_ENV,
+    "l144.png",
+  );
+  res.sendFile(imagePath, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Image not found");
+    }
+  });
+});
+
+app.get("/icons/l512.png", (req: Request, res: Response) => {
+  const imagePath = path.join(
+    __dirname,
+    "icons",
+    envVars.VITE_WHITE_ENV === "prod" ? "" : envVars.VITE_WHITE_ENV,
+    "l512.png",
+  );
+  res.sendFile(imagePath, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(404).send("Image not found");
+    }
+  });
+});
 
 app.get("*", (req: Request, res: Response) => {
   fs.readFile(indexPath, "utf8", (err: any, htmlData: any) => {
@@ -30,19 +68,10 @@ app.get("*", (req: Request, res: Response) => {
 
     const finalHtml = htmlData.replace("%ENV_VARIABLES%", envVariables);
 
-    // Serve the modified HTML
     res.send(finalHtml);
   });
 });
 
-app.get("/manifest.json", (req: Request, res: Response) => {
-  res.setHeader("Content-Type", "application/json");
-  res.send(
-    JSON.stringify(
-      manifest(envVars.VITE_WHITE_ENV === "preprod" ? "PreProd" : undefined),
-    ),
-  );
-});
 const port = 4100;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
